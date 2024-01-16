a98186a9d64a228c9e7010ce60f5801f9798c999
Ravi <rathoreravi1508@gmail.com>
2024-01-16 13:09:40 +0530

    add mongoose connnectin

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const { port, env } = require('./config/vars');
 const logger = require('./config/logger');
 const app = require('./config/express');
 const mongoose = require('./config/mongoose');
-// // TODO: mongoose
 
 // open mongoose connection
 mongoose.connect();

2c71befcf67352c605814d400fe5f2228adc6520
Ravi Rathore <rathoreravi1508@gmail.com>
2024-07-26 19:17:29 +0530

    fix user details in question controller

--- src/api/controllers/question.controller.js
+++ src/api/controllers/question.controller.js
@@ -1,80 +1,88 @@
 // Import necessary modules or models
-const httpStatus = require('http-status/lib');
-const Task = require('../models/task.model');
-const User = require('../models/user.model');
-const { Question } = require('../models/question.model');
+const httpStatus = require("http-status/lib");
+const Task = require("../models/task.model");
+const User = require("../models/user.model");
+const { Question } = require("../models/question.model");
 
 const questionController = {
-  // Controller function to add a question to a task
-  addQuestionToTask: async (req, res, next) => {
-    try {
-      const { taskId } = req.params;
-      const userId = req.user;
-      const { question } = req.body;
-      const { name } = await User.findById;
-      // Find the task by taskId
-      const task = await Task.findById(taskId);
-      if (!task) {
-        return res.status(404).json({ error: 'Task not found' });
-      }
+	// Controller function to add a question to a task
+	addQuestionToTask: async (req, res, next) => {
+		try {
+			const { taskId } = req.params;
+			const userId = req.user;
+			const { question } = req.body;
+			const user = await User.findById(userId);
+			if (!user) {
+				throw new Error("User not found");
+			}
+			const { firstName, lastName } = user;
+			// Find the task by taskId
+			const task = await Task.findById(taskId);
+			if (!task) {
+				return res.status(404).json({ error: "Task not found" });
+			}
+			console.log("log", task.postedBy.toString(), userId.toString());
+			if (task.postedBy.toString() === userId.toString()) {
+				throw new Error("You cannot ask a question on your own task");
+			}
 
-      // Create a new question
-      const newQuestion = new Question({
-        userId,
-        name,
-        message: question,
-      });
+			// Create a new question
+			const newQuestion = new Question({
+				userId,
+				name: `${firstName} ${lastName}`,
+				message: question,
+			});
 
-      // Save the question to the database
-      await newQuestion.save();
+			// Save the question to the database
+			await newQuestion.save();
 
-      // Add the question to the task's questions array
-      task.questions.push(newQuestion);
-      await task.save();
+			// Add the question to the task's questions array
+			task.questions.push(newQuestion);
+			await task.save();
 
-      res
-        .status(201)
-        .json({ message: 'Question added to task successfully', newQuestion });
-    } catch (error) {
-      next(error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  },
+			res
+				.status(201)
+				.json({ message: "Question added to task successfully", newQuestion });
+		} catch (error) {
+			next(error);
+			res.status(500).json({ error: "Internal server error" });
+		}
+	},
 
-  // Controller function to add a reply to a question
-  addReplyToQuestion: async (req, res, next) => {
-    try {
-      const { taskId, questionId } = req.params;
-      const { message } = req.body;
-      // Find the task by taskId
-      const task = await Task.findById(taskId);
-      if (!task) {
-        return res.status(404).json({ error: 'Task not found' });
-      }
-      // Find the question by questionId within the task
-      const question = task.questions.id(questionId);
-      if (!question) {
-        return res.status(404).json({ error: 'Question not found' });
-      }
+	// Controller function to add a reply to a question
+	addReplyToQuestion: async (req, res, next) => {
+		try {
+			const { taskId, questionId } = req.params;
+			const { message } = req.body;
+			// Find the task by taskId
+			const task = await Task.findById(taskId);
+			if (!task) {
+				return res.status(404).json({ error: "Task not found" });
+			}
+			// Find the question by questionId within the task
+			const question = task.questions.id(questionId);
+			if (!question) {
+				return res.status(404).json({ error: "Question not found" });
+			}
 
-      if (question.reply) {
-        return res
-          .status(httpStatus.BAD_REQUEST)
-          .json({ message: 'You have already replied' });
-      }
-      // Add the reply to the question's replies array
-      question.reply = {
-        userId: req.user,
-        message,
-      };
-      await task.save();
+			if (question.reply) {
+				return res
+					.status(httpStatus.BAD_REQUEST)
+					.json({ message: "You have already replied" });
+			}
+			// Add the reply to the question's replies array
+			question.reply = {
+				userId: req.user,
+				message,
+			};
+			await task.save();
 
-      res.status(201).json({ message: 'Reply added to question successfully' });
-    } catch (error) {
-      next(error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  },
+			res.status(201).json({ message: "Reply added to question successfully" });
+		} catch (error) {
+			next(error);
+			res.status(500).json({ error: "Internal server error" });
+		}
+	},
 };
 
 module.exports = questionController;
